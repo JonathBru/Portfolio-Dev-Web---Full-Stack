456e4c6968813ac63cf65cad7c06ddb548295e36
Bru Jonathan <jonathan.bru.marc@gmail.com>
2023-09-28 13:08:39 +0200

    functionnal

--- animations/modal.js
+++ animations/modal.js
@@ -1,52 +1,70 @@
-// Fonction pour créer une modale
-function createModal(modalId, modalTitle, modalImage1, modalImage2, text1, text2) {
+
+function createModal(modalId, modalTitle, modalImage1, modalImage2, text1, text2, link) {
     const modalContainer = document.getElementById("modalContainer");
 
-    // Créer la structure HTML de la modale
     const modal = document.createElement("div");
     modal.id = modalId;
     modal.className = "modal";
 
     const modalContent = `
         <div class="modal-content">
-            <span class="close-modal" onclick="closeModal('${modalId}')">&times;</span>
-            <h2>${modalTitle}</h2>
-            <div class="modal-images">
-                <img src="${modalImage1}" alt="${modalTitle} Image 1">
-                <img src="${modalImage2}" alt="${modalTitle} Image 2">
-            </div>
-            <div class="modal-text">
-                <p>${text1}</p>
-                <p>${text2}</p>
-            </div>
+                <div class="modal-container">
+                    <span class="close-modal" onclick="closeModal('${modalId}')">&times;</span>
+                    <h2>${modalTitle}</h2>
+                    <div class="modal-images">
+                        <img src="${modalImage1}" alt="${modalTitle} Image 1">
+                        <img src="${modalImage2}" alt="${modalTitle} Image 2">
+                    </div>
+                    <div class="modal-text">
+                        <div class="Tech-modal">
+                            <h3>Technologies</h3>
+                            <p>${text1}</p>
+                        </div>
+                        <div class="mission">
+                            <h3>Réalisation</h3>    
+                            <p>${text2}</p>
+                            <a href="${link}" target="_blank" class="modal-link">Voir le projet</a>
+                        </div>
+                    </div>
+
+                </div>
         </div>
     `;
 
     modal.innerHTML = modalContent;
 
-    // Ajouter la modale au conteneur
     modalContainer.appendChild(modal);
 }
 
 // Appeler createModal pour chaque modale avec des données différentes
-createModal("booki1", "Modale Booki", "booki-1.jpg", "booki-2.jpg", "Texte 1 pour Booki", "Texte 2 pour Booki");
-createModal("sophie1", "Modale Sophie Bluel", "sophiebluel1.jpg", "sophiebluel2.jpg", "Texte 1 pour Sophie Bluel", "Texte 2 pour Sophie Bluel");
-createModal("carducci1", "Modale Nina Carducci", "carducci-1.jpg", "carducci-2.jpg", "Texte 1 pour Nina Carducci", "Texte 2 pour Nina Carducci");
-createModal("kasa1", "Modale Kasa", "kasa-1.jpg", "kasa-2.jpg", "Texte 1 pour Kasa", "Texte 2 pour Kasa");
+createModal("booki1", "Modale Booki", "images/modale-images/booki-1.png", "images/modale-images/booki-2.png", "HTML/CSS", "Projet réalisé dans le cadre de ma formation OpenClassrooms de développeur Full-stack.<br>Mise en place d'une interface responsive pour le site web Booki.","https://github.com/JonathBru/JonathBru-BRU_JONATHAN_2_DOSSIERP2_022023");
+createModal("sophie1", "Modale Sophie Bluel", "images/modale-images/sophiebluel1.png", "images/modale-images/sophiebluel2.jpg", "HTML/CSS/JavaScript", "Projet réalisé dans le cadre de ma formation OpenClassrooms de développeur Full-stack.<br>Création du Front-end d'un site vitrine, conprenant les liaisons avec une API côté Front.","https://github.com/JonathBru/P3");
+createModal("carducci1", "Modale Nina Carducci", "images/modale-images/carducci-1.png", "images/modale-images/carducci-2.jpg", "HTML/CSS/JavaScript/Bootstrap<br>LightHouse/Purgecss/Gimp", "Projet réalisé dans le cadre de ma formation OpenClassrooms de développeur Full-stack.<br>Optimisation d'un site web préexistant, en therme de SEO, GreenCode, et Performance.","https://github.com/JonathBru/nina-carducciP5");
+createModal("kasa1", "Front-end du site Kasa avec React", "images/modale-images/kasa-1.png", "images/modale-images/kasa-2.png", "React/SCSS/HTML", "Projet de la formation OpenClassrooms.<br>Réalisation d'un site de location d'hebergement sous React.<br>L'arborescence sous forme de composants React à été utilisé pour rendre le code modulable.","https://github.com/JonathBru/P6_OpenClassRooms");
 
-// Fonction pour ouvrir une modale
+
+let isModalOpen = false;
+
 function openModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = "block";
+
+    modal.classList.remove("modal-close");
+    modal.classList.add("modal-open");
+
+    isModalOpen = true;
 }
 
-// Fonction pour fermer une modale
 function closeModal(modalId) {
     const modal = document.getElementById(modalId);
-    modal.style.display = "none";
+
+    modal.classList.remove("modal-open");
+    modal.classList.add("modal-close");
+
+
+    isModalOpen = false;
 }
 
-// Associer la fonction openModal à chaque carte
 const cards = document.querySelectorAll(".card");
 cards.forEach((card) => {
     card.addEventListener("click", function () {
@@ -54,3 +72,18 @@ cards.forEach((card) => {
         openModal(modalId);
     });
 });
+
+function closeAnywhereModal() {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => {
+        modal.addEventListener("click", function (event) {
+            if (event.target === modal) {
+                const modalId = modal.id;
+                closeModal(modalId);
+            }
+        });
+    });
+}
+
+closeAnywhereModal();
+
--- animations/nav.js
+++ animations/nav.js
@@ -4,14 +4,42 @@ const navbar = document.getElementsByClassName("pc-nav")[0];
 window.onscroll = () => {
 	const scrollY = window.scrollY;
 
-  	if (scrollY > 1100) {
-	  // Désactive la classe nav-active lorsque le défilement est entre 800 et 1200 pixels
+	if (scrollY > 1800) {
+		navbar.classList.add('nav-active');
+	} else if (scrollY > 1100) {
 	  navbar.classList.remove('nav-active');
 	} else if (scrollY > 500) {
-	  // Réactive la classe nav-active lorsque le défilement atteint 500 pixels
 	  navbar.classList.add('nav-active');
 	} else {
-	  // Désactive la classe nav-active lorsque le défilement est inférieur à 500 pixels
 	  navbar.classList.remove('nav-active');
 	}
-  };
+};
+
+
+
+
+//Burger
+const menuToggle = document.getElementById("menu-toggle");
+
+
+const toggleMenu = document.getElementById("toggle-menu");
+
+menuToggle.addEventListener("click", function () {
+
+    if (toggleMenu.style.display === "block") {
+        toggleMenu.style.display = "none";
+    } else {
+        toggleMenu.style.display = "block";
+    }
+});
+
+const menuItems = document.querySelectorAll("#toggle-menu ul li a");
+
+// Ajoutez un gestionnaire d'événements au clic sur chaque rubrique
+menuItems.forEach((menuItem) => {
+    menuItem.addEventListener("click", function () {
+        // Masquez le menu déroulant
+        toggleMenu.style.display = "none";
+    });
+});
+	
